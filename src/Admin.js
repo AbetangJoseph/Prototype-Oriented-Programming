4b58bc57b1c51bda1e91c148ae599c536beb636a
Abetang Joseph <josephabetang@gmail.com>
2019-04-17 15:23:55 +0100

    Create Admin Object and point prototype to User Object

--- src/Admin.js
+++ src/Admin.js
@@ -1,1 +1,10 @@
 let DB = require("./tempDB");
+let User = require("./Users");
+
+function Admin(name, email, password) {
+  User.call(this, name, email, password); //APPLYING THE INSTANCE VARIABLES OF USER OBJECT ON ADMIN
+  this.isAdmin = true;
+}
+
+Admin.prototype = Object.create(User.prototype);
+Admin.prototype.constructor = Admin;
--- src/Users.js
+++ src/Users.js
@@ -1,8 +1,10 @@
 let DB = require("./tempDB");
 
+//ID
 let id = 0;
 
 function User(name, email, password) {
+  //GENERATING USER ID STARTING FROM 1
   function idGenerator() {
     id == 0 ? (id = 1) : (id = ++id);
     return id;
@@ -13,3 +15,5 @@ function User(name, email, password) {
   this.isAdmin = false;
   this.id = idGenerator();
 }
+
+module.exports = User;
